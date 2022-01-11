57211178d4e9b3bccfb1bb46229cc084c58905b3
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-01-11 20:25:36 +0100

    Scaffolded Customer entity and dbcontext

---
+++ CarRental.Core/Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRental.Core.Domain.Entities
+{
+    public partial class Customer
+    {
+        public Guid Id { get; set; }
+        public string IdentityNumber { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; } = null!;
+    }
+}
---
+++ CarRental.Infrastructure/Data/Database/CarRentalDbContext.cs
@@ -0,0 +1,62 @@
+﻿using CarRental.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Infrastructure.Data.Database
+{
+    public partial class postgresContext : DbContext
+    {
+        public postgresContext()
+        {
+        }
+
+        public postgresContext(DbContextOptions<postgresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=abcd1234");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.DateOfBirth).HasColumnName("dateOfBirth");
+
+                entity.Property(e => e.IdentityNumber)
+                    .HasColumnType("character varying")
+                    .HasColumnName("identityNumber");
+
+                entity.Property(e => e.Name)
+                    .HasColumnType("character varying")
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Surname)
+                    .HasColumnType("character varying")
+                    .HasColumnName("surname");
+
+                entity.Property(e => e.TelephoneNumber)
+                    .HasColumnType("character varying")
+                    .HasColumnName("telephoneNumber");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
