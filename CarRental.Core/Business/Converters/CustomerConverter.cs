381ef2c2634e493892e4f75c7e169c9228862f8d
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-01-11 21:42:36 +0100

    Added CustomerDto and Converter

---
+++ CarRental.Core/Business/Converters/CustomerConverter.cs
@@ -0,0 +1,23 @@
+﻿using CarRental.Core.Business.Dtos;
+using CarRental.Core.Domain.Entities;
+
+namespace CarRental.Core.Business.Converters
+{
+    public static class CustomerConverter
+    {
+        public static CustomerDto ModelToDto(Customer customer)
+        {
+            if (customer == null) return new CustomerDto();
+
+            return new CustomerDto
+            {
+                Id = customer.Id,
+                IdentityNumber = customer.IdentityNumber,
+                Name = customer.Name,
+                Surname = customer.Surname,
+                DateOfBirth = customer.DateOfBirth,
+                TelephoneNumber = customer.TelephoneNumber
+            };
+        }
+    }
+}
---
+++ CarRental.Core/Business/Dtos/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Core.Business.Dtos
+{
+    public class CustomerDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string IdentityNumber { get; set; } = null!;
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Surname { get; set; } = null!;
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string TelephoneNumber { get; set; } = null!;
+    }
+}
