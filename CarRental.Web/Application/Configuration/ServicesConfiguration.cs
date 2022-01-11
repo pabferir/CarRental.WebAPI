63c9075dcfcb24df5c31da70bd009c7db1fe1119
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-01-11 20:35:12 +0100

    Registered DbCotext in services container

---
+++ CarRental.Web/Application/Configuration/ServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.Infrastructure.Data.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Web.Application.Configuration
+{
+    public static class ServicesConfiguration
+    {
+        public static void SetupServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            SetupDatabase<CarRentalDbContext>(services, configuration, "CarRentalPostgreSQL");
+        }
+
+        private static void SetupDatabase<T>(IServiceCollection services, IConfiguration configuration, string connectionStringName) where T : DbContext
+        {
+            services.AddDbContext<T>(options => options.UseNpgsql(configuration.GetConnectionString(connectionStringName)));
+        }
+    }
+}
--- CarRental.Web/Program.cs
+++ CarRental.Web/Program.cs
@@ -1,3 +1,5 @@
+using CarRental.Web.Application.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Custom services
+builder.Services.SetupServices(builder.Configuration);
+#endregion
 
 var app = builder.Build();
 
