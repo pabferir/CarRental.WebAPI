99ccf4fe42b3291a744311c222a161a44f296217
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-01-20 14:16:16 +0100

    Refactored to use Customer repository

--- src/CarRental.Web/Application/Controllers/CustomersController.cs
+++ src/CarRental.Web/Application/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using CarRental.Core.Domain.Entities;
+using CarRental.Core.Domain.RepositoryInterfaces;
 using CarRental.Infrastructure.Data.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,105 +10,71 @@ namespace CarRental.Web.Application.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        private readonly CarRentalDbContext _context;
+        private readonly ICustomerRepository _customerRepository;
 
-        public CustomersController(CarRentalDbContext context)
+        public CustomersController(ICustomerRepository customerRepository)
         {
-            _context = context;
+            _customerRepository = customerRepository;
         }
 
         [HttpPost]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> AddCustomer(Customer customer)
         {
-            var result = await _context.Customers.AddAsync(customer).ConfigureAwait(false);
-            result.State = EntityState.Added;
-            await _context.SaveChangesAsync().ConfigureAwait(false);
-            result.State = EntityState.Detached;
-            return StatusCode(StatusCodes.Status201Created, customer);
+            var result = await _customerRepository.InsertCustomer(customer).ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status201Created, result);
         }
 
         [HttpGet]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(IList<Customer>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAllCustomers()
         {
-            var result = await _context.Customers.ToListAsync().ConfigureAwait(false);
-            return StatusCode(StatusCodes.Status201Created, result);
+            var result = await _customerRepository.GetCustomer().ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status200OK, result);
         }
 
         [HttpGet("/{id}")]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetCustomerById(Guid id)
         {
-            var result = await _context.Customers.FirstOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
-            if (result == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, id);
-            }
-            return StatusCode(StatusCodes.Status201Created, result);
+            var result = await _customerRepository.GetCustomer(customer => customer.Id == id).ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status200OK, result);
         }
 
         [HttpPut("/{id}")]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> EditCustomer(Customer customer)
         {
-            var result = _context.Set<Customer>().Attach(customer);
-            _context.Entry(customer).State = EntityState.Modified;
-            await _context.SaveChangesAsync().ConfigureAwait(false);
-            result.State = EntityState.Detached;
-            return Ok();
+            var result = await _customerRepository.UpdateCustomer(customer).ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status200OK, result);
 
         }
 
         [HttpDelete]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteAllCustomers()
         {
-            var deleted = 0;
-            var customers = await _context.Customers.ToListAsync().ConfigureAwait(false);
-            if (!customers.Any())
-            {
-                return Ok();
-            }
-            foreach (var customer in customers)
-            {
-                var result = _context.Customers.Remove(customer);
-                result.State = EntityState.Deleted;
-                deleted = await _context.SaveChangesAsync().ConfigureAwait(false);
-                result.State = EntityState.Detached;
-                if (deleted <= 0)
-                {
-                    StatusCode(StatusCodes.Status500InternalServerError, deleted > 0);
-                }
-            }
-            return StatusCode(StatusCodes.Status200OK, deleted > 0);
+            var result = await _customerRepository.DeleteCustomer().ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status200OK, result);
         }
 
         [HttpDelete("/{id}")]
-        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteCustomerById(Guid id)
         {
-            var customer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == id).ConfigureAwait(false);
-            if (customer == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, id);
-            }
-            var result = _context.Customers.Remove(customer);
-            result.State = EntityState.Deleted;
-            var deleted = await _context.SaveChangesAsync().ConfigureAwait(false);
-            result.State = EntityState.Detached;
-            return StatusCode(StatusCodes.Status200OK, deleted > 0);
+            var result = await _customerRepository.DeleteCustomer(customer => customer.Id == id).ConfigureAwait(false);
+            return StatusCode(StatusCodes.Status200OK, result);
         }
     }
 }
