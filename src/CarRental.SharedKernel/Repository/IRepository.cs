a13fdab8bce5ff05b93cbe7405ceb6ff251e71b1
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-01-28 09:15:05 +0100

    Refactored Repositories to allow SaveChanges from UnitOfWork

--- src/CarRental.SharedKernel/Repository/IRepository.cs
+++ src/CarRental.SharedKernel/Repository/IRepository.cs
@@ -4,10 +4,10 @@ namespace CarRental.SharedKernel.Repository
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        Task<TEntity> Insert(TEntity entity);
-        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> filter);
-        Task<TEntity> Update(TEntity entity);
-        Task<bool> Delete(TEntity entity);
-        Task<bool> Delete(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> Insert(TEntity entity, bool saveChanges);
+        Task<IEnumerable<TEntity>> GetWhere(Expression<Func<TEntity, bool>>? filter);
+        Task<TEntity> Update(TEntity entity, bool saveChanges);
+        Task<bool> Delete(TEntity entity, bool saveChanges);
+        Task<bool> DeleteWhere(Expression<Func<TEntity, bool>>? filter, bool saveChanges);
     }
 }
