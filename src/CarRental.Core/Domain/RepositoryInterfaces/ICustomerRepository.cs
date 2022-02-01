a563929f82894727e3a980d69719285390e15e74
Pablo Ferrando <pablo_f.i@hotmail.com>
2022-02-01 19:27:24 +0100

    Refactored methods and parameters

--- src/CarRental.Core/Domain/RepositoryInterfaces/ICustomerRepository.cs
+++ src/CarRental.Core/Domain/RepositoryInterfaces/ICustomerRepository.cs
@@ -6,10 +6,9 @@ namespace CarRental.Core.Domain.RepositoryInterfaces
 {
     public interface ICustomerRepository : IRepository<Customer>
     {
-        Task<Customer> InsertCustomer(Customer customer, bool saveChanges);
+        Task<Customer> InsertCustomer(string identityNumber, string name, string surname, DateTime dateOfBirth, string telephoneNumber, bool saveChanges);
         Task<IEnumerable<Customer>> GetCustomerWhere(Expression<Func<Customer, bool>>? filter);
-        Task<Customer> UpdateCustomer(Customer customer, bool saveChanges);
-        Task<bool> DeleteCustomer(Customer customer, bool saveChanges);
+        Task<Customer> UpdateCustomer(Guid id, string identityNumber, string name, string surname, DateTime dateOfBirth, string telephoneNumber, bool saveChanges);
         Task<bool> DeleteCustomerWhere(Expression<Func<Customer, bool>>? filter, bool saveChanges);
     }
 }
